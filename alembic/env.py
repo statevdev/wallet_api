5c375404a115ec987c0c08cc506780616e20e1f1
Alexey Statev <statev.dev@gmail.com>
2026-07-17 09:52:47 +0600

    Move database url to alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -3,10 +3,12 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from app.config import DATABASE_URL
 from app.database import Base
 from app.models import Wallet
 
 config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
